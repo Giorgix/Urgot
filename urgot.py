eba94d239f7cc4f6cb394ece3954965ff201a502
Giorgix <jackosinapsis@gmail.com>
2013-02-01 05:48:14 +0100

    Test de conexion basico

--- urgot.py
+++ urgot.py
@@ -1,1 +1,19 @@
 # Urgot email client
+import poplib
+class Connection(object):
+	
+    def connection_pop3(self, user, password):
+        print 'Connecting...'
+	host = 'pop.gmail.com'
+        pop_conn=poplib.POP3_SSL(host)
+        pop_conn.user(user)
+        pop_conn.pass_(password)
+        print pop_conn.getwelcome()
+        return pop_conn
+
+
+
+if __name__=='__main__':
+   connection = Connection()
+   connection.connection_pop3('user', 'password')
+
--- urgot_test.py
+++ urgot_test.py
@@ -1,1 +1,14 @@
-#Urgot email client Test
+import unittest
+from urgot import Connection
+
+class connection_pop3Test(unittest.TestCase):
+    
+    def setUp(self):
+        self.connection = Connection()
+    def test_connection_pop3(self):
+
+	self.assertTrue(self.connection.connection_pop3('user', 'password'))
+	
+
+if __name__ == '__main__':
+    unittest.main()
