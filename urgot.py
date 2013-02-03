50d08c91b65f2e1baea9bae3af7f58602beb8382
Giorgix <jackosinapsis@gmail.com>
2013-02-03 19:20:34 +0100

    conexion y comprobbacion de login

--- urgot.py
+++ urgot.py
@@ -1,6 +1,7 @@
 # Urgot email client
 import sys, shelve, getpass, string, os, email
 import smtplib, poplib
+import re
 class Connection(object):
 	
     def connection_pop3(self, user, password):
@@ -20,18 +21,27 @@ class Check_username(object):
     
 
     def get_user(self):
-        user = ask('Your email address: ')
-	if check_mail_addr_pop(user):
+
+        user = self.ask('Your email address: ')
+
+	if self.check_mail_addr_pop(user):
 	    return user
+
         else:
 	    print 'Invalid email, try again:'
-	    return get_user()
+	    return self.get_user()
+
+    def get_password(self):
+        
+	password = getpass.getpass('enter you password for %s: '%user)
+	return password
     
     
     
     def check_mail_addr_pop(self, addr):
-	alfa_num = compile('([A-Z] * [a-z] * [0-9] *) + @ ([A-Z] * [a-z] * [0-9] *) + . ([A-	    Z] * [a-z] * [0-9] *)')
-    	return alfa_num.search(addr)
+	
+	alfa_num = re.compile('([A-Z]*[a-z]*[0-9]*)+@([A-Z]*[a-z]*[0-9]*)+.([A-Z]*[a-z]*[0-9]*)')
+	return alfa_num.search(addr)
 
 
 
@@ -39,5 +49,6 @@ if __name__=='__main__':
    connection = Connection()
    check_username = Check_username()
    user = check_username.get_user()
-   connection.connection_pop3('user', 'password')
+   password = check_username.get_password()
+   connection.connection_pop3(user, password)
 
