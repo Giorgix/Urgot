638d8d9a2726181fac6456fce732c2e5a05e35f0
Giorgix <jackosinapsis@gmail.com>
2013-02-04 19:34:25 +0100

    3 commit, lista los correos nuevos

--- urgot.py
+++ urgot.py
@@ -2,6 +2,8 @@
 import sys, shelve, getpass, string, os, email
 import smtplib, poplib
 import re
+from email import parser
+
 class Connection(object):
 	
     def connection_pop3(self, user, password):
@@ -13,7 +15,7 @@ class Connection(object):
         print pop_conn.getwelcome()
         return pop_conn
 
-class Check_username(object):
+class Login(object):
 
     def ask(self, question):
         print "\r%s" %question,
@@ -44,11 +46,29 @@ class Check_username(object):
 	return alfa_num.search(addr)
 
 
+class MailActions(object):
+
+    def list_mail(self, connection):
+        msgs_list = [connection.retr(i) for i in range(1,len(connection.list()[1])+1)  ]
+	msgs_list =['\n'.join(m[1]) for m in msgs_list]
+	msgs_list = [parser.Parser().parsestr(m) for m in msgs_list]
+	return msgs_list
+
+    def print_mail_list(self, messages):
+        print 'Inbox: %s' % len(messages)
+	i=0
+	while i<len(messages): 
+	    print '['+str(i+1)+'] ',messages[i]['subject']
+	    i=i+1
+	
+
 
 if __name__=='__main__':
    connection = Connection()
-   check_username = Check_username()
-   user = check_username.get_user()
-   password = check_username.get_password()
-   connection.connection_pop3(user, password)
-
+   login = Login()
+   mail_actions = MailActions()
+   user = login.get_user()
+   password = login.get_password()
+   pop3_conn = connection.connection_pop3(user, password)
+   messages = mail_actions.list_mail(pop3_conn)
+   mail_actions.print_mail_list(messages)
